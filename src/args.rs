7c688976b16f7731b0a8dea903843f0189ba6187
ikyro <ikyroch@gmail.com>
2023-01-22 16:14:20 -0300

    feat: easy to use

--- src/args.rs
+++ src/args.rs
@@ -1,30 +1,16 @@
-use clap::{Parser, Subcommand};
+use clap::Parser;
 
 #[derive(Parser, Debug)]
 pub struct Args {
-    #[command(subcommand)]
-    pub command: Option<Commands>,
+    /// Path where be downloaded doujin by default is `~/.nhentai/`
+    #[arg(short, long, default_value_t = false)]
+    pub path: bool,
 
-    /// Path where be downloaded doujin
-    #[arg(short, long)]
-    pub path: Option<std::path::PathBuf>,
-
-    /// Force ignoreing cache
-    #[arg(short, long)]
-    pub force: Option<bool>,
+    /// Force download doujin
+    #[arg(short, long, default_value_t = false)]
+    pub force: bool,
 
     /// Nhentai code
     #[arg(short, long)]
     pub code: i64,
-}
-
-#[derive(Subcommand, Debug)]
-pub enum Commands {
-    /// Show config file
-    Config {
-        show: Option<bool>,
-
-        /// Set csfrtoken and user-agent from `config.json` file
-        edit: Option<bool>,
-    },
 }
