0a1045d71521c3d1894aad76c7b05ec06f6e320d
ikyro <ikyroch@gmail.com>
2023-01-22 16:16:13 -0300

    feat: add optional path

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,61 @@ use args::Args;
 use clap::Parser;
 use config::Config;
 use nhentai::Nhentai;
+use requestty::Question;
+use std::path::{Path, PathBuf};
 
 mod args;
 mod config;
 mod nhentai;
 
-fn main() {
+fn get_custom_path() -> PathBuf {
+    let default_path = format!("{}", dirs::home_dir().unwrap().join(".nhentai").display());
+    let ask_path = Question::input("path")
+        .message("Where do you want to download the doujin?")
+        .default(default_path)
+        .validate(|path, _| {
+            let path = Path::new(path);
+
+            match path.exists() {
+                true => Ok(()),
+                false => Err("Path doesn't exist".to_string()),
+            }
+        })
+        .build();
+    let answer = requestty::prompt_one(ask_path).unwrap();
+    let path = Path::new(answer.as_string().unwrap()).to_path_buf();
+
+    path
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Args::parse();
     let Config {
         user_agent,
         csrftoken,
         nhentai_path,
-    } = Config::new().unwrap();
-    let doujin = Nhentai::new(cli.code, user_agent, csrftoken);
+    } = Config::new()?;
+    let doujin = Nhentai::new(cli.code, user_agent, csrftoken).await;
+    let path = match cli.path {
+        true => get_custom_path(),
+        false => nhentai_path.parent().unwrap().to_path_buf(),
+    };
 
-    println!("{}", doujin.get_title());
+    if cli.force {
+        doujin.build(path).await;
+    } else {
+        let confirm = Question::confirm("confirm")
+            .default(false)
+            .message(format!("Do you want to download {}?", doujin.get_title()))
+            .build();
 
-    doujin.build(nhentai_path);
+        let answer = requestty::prompt_one(confirm)?;
+
+        if answer.as_bool().unwrap() {
+            doujin.build(path).await;
+        }
+    }
+
+    Ok(())
 }
